676567220307b5ef527df6986f0986e81e926f8d
Jelena Smiljanic <jelenasmiljanic3@gmail.com>
2020-02-20 21:11:42 +0100

    The Basics day 2 home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import {ServerComponent} from "./server/server.component";
+import {ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
