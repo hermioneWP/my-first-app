3cb3a61bf97984807686551c16fc6cd7cb650bee
Jelena Smiljanic (TAASMJE1) <jelena.smiljanic@swisscom.com>
2020-02-28 16:51:55 +0100

    components & databinding day 2 part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   serverElements = [{type:'server',name:'Test Server',content:'Just a test!'}];
 
+  onServerAdded(serverData:{serverName:string,serverContent:string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onABlueprintAdded(bluePrintData:{serverName:string,serverContent:string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent
+    });
+  }
 }
