ec2a623e7ae4c5a0ee91e7b9bc388abb3d64f294
Jelena Smiljanic (TAASMJE1) <jelena.smiljanic@swisscom.com>
2020-03-23 18:47:40 +0100

    routing gotov

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -30,6 +30,10 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
+  getRecipe(id: number) {
+    return this.recipes[id];
+  }
+
   constructor(private shoppingListService: ShoppingListService) {
   }
 
